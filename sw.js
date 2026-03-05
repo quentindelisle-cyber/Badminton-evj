51149f1ce66d63cdee61c25e424993361e8524d4
quentindelisle-cyber <quentin.delisle@gmail.com>
2026-03-05 17:53:31 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+/* EVJ Badminton - Service Worker */
+const CACHE_NAME = "evj-badminton-pwa-v1";
+const ASSETS = [
+  "./EVJ%20Badminton.html",
+  "./manifest.webmanifest",
+  "./offline.html",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(req).then((res) => {
+        if (req.method === "GET" && res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, clone));
+        }
+        return res;
+      }).catch(() => caches.match("./offline.html"));
+    })
+  );
+});
